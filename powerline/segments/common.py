0f17b22d0e6387a35fc0e299b8a703d0f9715117
sanmiguel <michael.coles@gmail.com>
2014-08-26 15:44:55 +0100

    Fix highlight_group docstring for kerl segment

--- powerline/segments/common.py
+++ powerline/segments/common.py
@@ -56,7 +56,7 @@ def hostname(pl, segment_info, only_if_ssh=False, exclude_domain=False):
 def kerl(pl, segment_info):
 	'''Return the in-use erlang installation (via kerl)
 
-	Highlight_groups used: ```kerl_active```
+	Highlight_groups used: ``kerl_active``.
 	'''
 	kerl_active = run_cmd(pl, ['kerl', 'prompt']) or None
 	if not kerl_active: return None
